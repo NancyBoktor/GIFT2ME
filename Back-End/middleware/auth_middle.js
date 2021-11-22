0f27317c8810c41aa4d6369440fdad7a262c5da6
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-22 10:24:52 -0500

    working on register feature.

--- Back-End/middleware/auth_middle.js
+++ Back-End/middleware/auth_middle.js
@@ -1,22 +1,19 @@
 const jwt = require("jsonwebtoken");
 
 const isAuth = (req, res, next) => {
-  console.log(req.cookies)
+  console.log(req.cookies);
   const { token } = req.cookies;
-  if (!token){
+  if (!token) {
     return res.status(401).json({ success: false, message: "Unauthorized !" });
   }
-  const user = jwt.verify(
-    token,
-    process.env.TOKEN_SECRET 
-  );
-  if (!user){
-       return res.status(401).json({ success: false, message: "Unauthorized !" });
+  const user = jwt.verify(token, process.env.TOKEN_SECRET);
+  if (!user) {
+    return res.status(401).json({ success: false, message: "Unauthorized !" });
   }
-  req.current_user_id = user.id
+  req.current_user_id = user.id;
   next();
-}
+};
 
-module.exports ={
-    isAuth
-}
+module.exports = {
+  isAuth,
+};
