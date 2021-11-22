5d1d1553e7e329dcf2711bd15477b9146b34fad5
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-22 13:24:31 -0500

    Install Classnames.

---
+++ Front-End/src/components/button.jsx
@@ -0,0 +1,19 @@
+import "components/Button.scss";
+import classNames from "classnames";
+
+export default function Button(props) {
+  let buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+}
