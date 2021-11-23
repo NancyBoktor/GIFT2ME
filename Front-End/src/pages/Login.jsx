c469ba5f4f0fadaa411f8828bb51e83caf6f20a9
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-23 13:22:06 -0500

    nothing new

--- Front-End/src/pages/Login.jsx
+++ Front-End/src/pages/Login.jsx
@@ -1,32 +1,32 @@
-import react, { useState } from 'react';
+import react, { useState } from "react";
 import { login } from "../services/auth";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./Login.scss";
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Login = () => {
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState({
     email: "",
-    password: ""
-  })
+    password: "",
+  });
 
   const [errorMsg, setErrorMsg] = useState("");
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
-      await login(userInfo)
-      navigate('/dashboard');
+      await login(userInfo);
+      navigate("/dashboard");
     } catch (e) {
-      console.log(e)
-      console.log(e.response.status)
-      console.log(e.response.data)
-      console.log(e.response.data.message)
-      setErrorMsg(e.response.data.message)
+      console.log(e);
+      console.log(e.response.status);
+      console.log(e.response.data);
+      console.log(e.response.data.message);
+      setErrorMsg(e.response.data.message);
     }
-  }
+  };
 
   return (
     <div id="login-container">
@@ -36,24 +36,39 @@ const Login = () => {
         <form onSubmit={handleLogin}>
           <label>
             <p>Email</p>
-            <input type="email" defaultValue={userInfo.email} onChange={(event) => setUserInfo({ ...userInfo, email: event.target.value })} autoComplete="off" />
+            <input
+              type="email"
+              defaultValue={userInfo.email}
+              onChange={(event) =>
+                setUserInfo({ ...userInfo, email: event.target.value })
+              }
+              autoComplete="off"
+            />
           </label>
           <label>
             <p>Password</p>
-            <input type="password" defaultValue={userInfo.password} onChange={(event) => setUserInfo({ ...userInfo, password: event.target.value })} autoComplete="off" />
+            <input
+              type="password"
+              defaultValue={userInfo.password}
+              onChange={(event) =>
+                setUserInfo({ ...userInfo, password: event.target.value })
+              }
+              autoComplete="off"
+            />
           </label>
           <span className="error-msg">{errorMsg}</span>
           <div>
-            <button type="submit" className="login-btn">Login</button>
+            <button type="submit" className="login-btn">
+              Login
+            </button>
           </div>
         </form>
-        <div>Don't have an account? |
-          <span> Register</span>
+        <div>
+          Don't have an account? |<span> Register</span>
         </div>
       </section>
       <Footer />
     </div>
-
-  )
-}
-export default Login
+  );
+};
+export default Login;
