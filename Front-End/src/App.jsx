dde171fc8d57a2396fa01e43a805c0faf4593bd0
Nancy Boktor <nancyboktor1@gmail.com>
2021-11-23 13:10:43 -0500

    Working on Navbar.

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
