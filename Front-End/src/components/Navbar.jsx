fe5ad595454a71843d171115a820330ff621ea1e
Maram Qdah <maram.m.qdah@gmail.com>
2021-11-22 20:42:37 -0500

    addeed register to navbar

--- Front-End/src/components/Navbar.jsx
+++ Front-End/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import react from 'react';
+import react, { useState } from 'react';
 import "./Navbar.scss";
+
 
 const Navbar = (props) => {
   return (
     <nav className='nav'>
       <span className='logo'>GIFT2ME</span>
-      <span className='span'>Login</span> 
+      <span className='span' >Login</span> 
       <span className='span'> Logout</span>
+      <span className='span' >Register</span> 
       </nav>
   )
 }
