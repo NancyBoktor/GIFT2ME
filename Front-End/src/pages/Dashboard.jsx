fc9ed3320b4b235a5bf9900ba73d1ebeb99d5f53
Shanna J. Smith <shannasmith@Shannas-Air.phub.net.cable.rogers.com>
2021-11-24 13:02:35 -0500

    delete commented out import

--- Front-End/src/pages/Dashboard.jsx
+++ Front-End/src/pages/Dashboard.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./Dashboard.scss";
-// import Navbar from "./Navbar.jsx";
-// import Footer from "./Footer";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { useNavigate } from 'react-router-dom';
