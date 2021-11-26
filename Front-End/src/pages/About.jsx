6a847dba441134805697161643654a370dd01a19
Shanna J. Smith <shannasmith@Shannas-Air.phub.net.cable.rogers.com>
2021-11-26 18:17:21 -0500

    fix about pages style

--- Front-End/src/pages/About.jsx
+++ Front-End/src/pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <>
       <Navbar />
       <div>
-      <h1>About</h1>
+      <h1 className="about">About</h1>
       <section className="about-text">
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
