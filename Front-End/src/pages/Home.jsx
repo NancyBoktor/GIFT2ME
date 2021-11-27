0619d63686075fa41265fae2872222cf54339f0b
Shanna J. Smith <shannasmith@Shannas-Air.phub.net.cable.rogers.com>
2021-11-26 22:27:57 -0500

    homepage styling

--- Front-End/src/pages/Home.jsx
+++ Front-End/src/pages/Home.jsx
@@ -1,25 +1,59 @@
 import react from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import "./Home.scss";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    pink: {
+      main: '#D11071;',
+    },
+  },
+});
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <h2>Home Page</h2>
-      <div>
-      <section className="">
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      </section>
+      <div className="top">
+
+        <div id="top-children">
+          <img className="hp-image" alt="bar" src="https://images.unsplash.com/photo-1510284876186-b1a84b94418f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" />
+        </div>
+        <div class="top-children phrase">Gifting made easy
+        </div>
+        <div id="regg">
+        <ThemeProvider theme={theme}>
+          <Link to="/register" className="reg-btn" style={{ textDecoration: 'none' }}>
+            <Button
+              variant="contained"
+              href="#contained-buttons"
+              color="pink"
+              size="large"
+            >
+              Register Now
+            </Button>
+          </Link>
+        </ThemeProvider>
+        </div>
+
       </div>
+
+      <div className="reasons">
+        <div id="circle"><p id="words">Register</p></div>
+        <div id="circle"><p id="words">Create Events</p></div>
+        <div id="circle"><p id="words">Make Wishlist</p></div>
+        <div id="circle"><p id="words">Share</p></div>
+      </div>
+
+      <div className="homepage-content">
+        <p>WORDS</p>
+      </div>
+
+
       <Footer />
     </>
   );
