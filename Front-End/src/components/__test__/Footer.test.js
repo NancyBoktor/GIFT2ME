781cf604304cb36554fc75f7a20091e04fa1b51d
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-06 12:51:03 -0500

    create testing folder and footer.test.js

---
+++ Front-End/src/components/__test__/Footer.test.js
@@ -0,0 +1,11 @@
+import { cleanup, screen, render } from '@testing-library/react';
+import Footer from '../Footer';
+
+afterEach(cleanup);
+
+describe('Footer', () => {
+  test('should render author names on page', () => {
+    render (<Footer />);
+    const names = 
+  })
+})
