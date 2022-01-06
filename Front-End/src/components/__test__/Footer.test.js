5ba14a4fcfa419357c0933a2482e0a879a18007b
Shanna J. Smith <shanna_smith987@hotmail.com>
2022-01-06 13:33:29 -0500

    add render footer test and pass it

--- Front-End/src/components/__test__/Footer.test.js
+++ Front-End/src/components/__test__/Footer.test.js
@@ -1,11 +1,25 @@
-import { cleanup, screen, render } from '@testing-library/react';
-import Footer from '../Footer';
+import { BrowserRouter } from 'react-router-dom'
+import { cleanup, screen, render } from '@testing-library/react'
+import Footer from '../Footer'
 
-afterEach(cleanup);
+afterEach(cleanup)
 
 describe('Footer', () => {
+  test('should render footer component without crashing', () => {
+    render(
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>,
+    )
+  })
+
   test('should render author names on page', () => {
-    render (<Footer />);
-    const names = 
+    render(
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>,
+    )
+    const authors = screen.getByText('Made with ❤️ Maram - Nancy - Shanna')
+    expect(authors).toBe('<p>Made with ❤️ Maram - Nancy - Shanna</p>')
   })
 })
